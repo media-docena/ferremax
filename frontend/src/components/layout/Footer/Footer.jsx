58ebfa8f947579a62a1fc99178b81d28366f4cb1
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2025-10-11 19:24:14 -0300

    agrega componente Footer

---
+++ frontend/src/components/layout/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="bg-transparent text-grey-600 py-8 mt-auto">
+            <div className="container mx-auto text-center">
+                <p className="text-sm">&copy; Ferremax 2025</p>
+                <p className="text-sm">Desarrollado por Media Docena Solutions</p>
+            </div>
+        </footer>   
+    )
+}
+export default Footer;
